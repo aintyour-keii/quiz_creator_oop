1c134d1d94b4af0f8a50e3c53f5ee4d578ff666d
aintyour-keii <klynepascual.edu@gmail.com>
2025-05-23 21:15:24 +0800

    QuizManager: init function created

--- classes/quiz_manager.py
+++ classes/quiz_manager.py
@@ -1,9 +1,11 @@
+import os
 from classes.quiz_creator import QuizCreator
 from classes.quiz_taker import QuizTaker
 
 class QuizManager:
     def __init__(self):
-        pass
+        self.quizzes_folder = "quizzes"
+        os.makedirs(self.quizzes_folder, exist_ok=True)
 
     def main_menu(self):
         pass
