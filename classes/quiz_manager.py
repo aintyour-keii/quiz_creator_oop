2af4ad87cedac095a89d32c7c4d65fd3a94ed3ad
aintyour-keii <klynepascual.edu@gmail.com>
2025-05-23 21:12:30 +0800

    temporary QuizManager class added

---
+++ classes/quiz_manager.py
@@ -0,0 +1,9 @@
+from classes.quiz_creator import QuizCreator
+from classes.quiz_taker import QuizTaker
+
+class QuizManager:
+    def __init__(self):
+        pass
+
+    def main_menu(self):
+        pass
